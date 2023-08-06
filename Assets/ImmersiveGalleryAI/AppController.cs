75cddd8aab15fcd68580da77ed5d92b595a326e8
Andrii Horlov <ahorlov@tsukat.com>
2023-08-06 07:57:38 +0100

    More readable scripts

--- Assets/ImmersiveGalleryAI/AppController.cs
+++ Assets/ImmersiveGalleryAI/AppController.cs
@@ -1,8 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using ImmersiveGalleryAI.Data;
 using ImmersiveGalleryAI.ImageHandler;
+using ImmersiveGalleryAI.Keyboard;
+using ImmersiveGalleryAI.User;
+using ImmersiveGalleryAI.VoiceRecognition;
 using UnityEngine;
 using Zenject;
 
@@ -12,16 +14,51 @@ namespace ImmersiveGalleryAI
     {
         [SerializeField] private List<WallImage> _images;
         
+        private WallImage _currentImagePanel;
+        private bool _isMicEnabled;
+
+        [Inject] private IVoiceHandler _voiceHandler;
         [Inject] private IDataManager _dataManager;
+        [Inject] private IKeyboard _keyboard;
+        [Inject] private IUser _user;
 
         private void Awake()
         {
             _dataManager.LoadSettings();
         }
 
+        private void OnEnable()
+        {
+            _voiceHandler.TranscriptionDoneEvent += RequestTranscriptEventHandler;
+            _voiceHandler.StoppedListeningEvent += StoppedListeningEventHandler;
+
+            foreach (WallImage wallImage in _images)
+            {
+                wallImage.OpenedPanel += OpenedPanelEventHandler;
+            }
+        }
+
         private void Start()
         {
             LoadPreviousImages();
+        }
+
+        private void OnDisable()
+        {
+            _voiceHandler.TranscriptionDoneEvent -= RequestTranscriptEventHandler;
+            _voiceHandler.StoppedListeningEvent -= StoppedListeningEventHandler;
+            
+            foreach (WallImage wallImage in _images)
+            {
+                wallImage.OpenedPanel -= OpenedPanelEventHandler;
+                wallImage.ControlPanel.InputFieldSelected -= InputFieldSelectedEventHandler;
+                wallImage.ControlPanel.VoiceClicked -= VoiceClickedEventHandler;
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            _dataManager.SaveSettings();
         }
 
         private void LoadPreviousImages()
@@ -41,9 +78,70 @@ namespace ImmersiveGalleryAI
             }
         }
 
-        private void OnApplicationQuit()
+        private void OpenedPanelEventHandler(WallImage wallImage)
         {
-            _dataManager.SaveSettings();
+            if (_currentImagePanel != null)
+            {
+                if (_currentImagePanel != wallImage)
+                {
+                    _currentImagePanel.HideControlPanel();
+                } 
+                
+                _currentImagePanel.ControlPanel.InputFieldSelected -= InputFieldSelectedEventHandler;
+                _currentImagePanel.ControlPanel.VoiceClicked -= VoiceClickedEventHandler;
+            }
+            
+            _currentImagePanel = wallImage;
+            _keyboard.Target = wallImage.ControlPanel.InputField;
+            wallImage.ControlPanel.InputFieldSelected += InputFieldSelectedEventHandler;
+            wallImage.ControlPanel.VoiceClicked += VoiceClickedEventHandler;
         }
+        
+        private void InputFieldSelectedEventHandler()
+        {
+            if (_keyboard.IsActive)
+            {
+                return;
+            }
+
+            _keyboard.ChangePosition(_user.CameraRigTransform);
+            _keyboard.SetActive(true);
+        }
+        
+#region Voice recognition
+
+        private void StoppedListeningEventHandler()
+        {
+            ToggleMicButton(false);
+        }
+
+        private void RequestTranscriptEventHandler(string transcript)
+        {
+            _currentImagePanel.ControlPanel.InputField.text = transcript;
+            ToggleMicButton(false);
+        }
+
+        private void ToggleMicButton(bool isActive)
+        {
+            _isMicEnabled = isActive;
+            _currentImagePanel.ControlPanel.ToggleMicText(isActive);
+        }
+
+        private void VoiceClickedEventHandler()
+        {
+            ToggleMicButton(!_isMicEnabled);
+
+            if (_isMicEnabled)
+            {
+                _voiceHandler.Activate();
+            }
+            else
+            {
+                ToggleMicButton(false);
+            }
+        }
+
+#endregion
+
     }
 }
