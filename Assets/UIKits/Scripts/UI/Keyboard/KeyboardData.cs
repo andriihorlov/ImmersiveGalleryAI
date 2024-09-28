47306d4fb853474ce1fa44aa16aff329f2bf1959
Andrii Horlov <andreygorlovv@gmail.com>
2024-09-28 12:47:03 +0100

    Make teleport possible

--- Assets/UIKits/Scripts/UI/Keyboard/KeyboardData.cs
+++ Assets/UIKits/Scripts/UI/Keyboard/KeyboardData.cs
@@ -22,6 +22,7 @@ namespace VRUiKits.Utils
         [field: SerializeField] public Button BackspaceButton { get; private set; }
 
         [Inject] private IKeyboard _keyboard;
+        
         public bool IsUpperCase
         {
             get => _isUppercase;
@@ -37,8 +38,9 @@ namespace VRUiKits.Utils
             if (_keyList?.Length < 1)
             {
                 Debug.Log($"Please fetch all items via ContextMenu");
+                return;
             }
-
+            
             _keyboard.Init(this);
         }
         
