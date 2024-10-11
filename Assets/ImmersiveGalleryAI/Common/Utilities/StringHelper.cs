8229d650ec835976078501b5f6975ffdaee36522
Andrii Horlov <andreygorlovv@gmail.com>
2024-10-11 18:04:15 +0100

    Add to StringHelper capitalize string

--- Assets/ImmersiveGalleryAI/Common/Utilities/StringHelper.cs
+++ Assets/ImmersiveGalleryAI/Common/Utilities/StringHelper.cs
@@ -27,5 +27,20 @@ namespace ImmersiveGalleryAI.Common.Utilities
         {
             return $"<b= {s} </b>";
         }
+        
+        public static string CapitalizeFirstLetter(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 0)
+                {
+                    words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
+                }
+            }
+            return string.Join(" ", words);
+        }
     }
 }
