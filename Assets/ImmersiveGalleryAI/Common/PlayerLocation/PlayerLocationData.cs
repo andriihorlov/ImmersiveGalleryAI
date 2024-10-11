d78c82ccd14cb96b4e352035b20f8654ca7df37a
Andrii Horlov <andreygorlovv@gmail.com>
2024-10-11 18:05:46 +0100

    Update Player Location manager

--- Assets/ImmersiveGalleryAI/Common/PlayerLocation/PlayerLocationData.cs
+++ Assets/ImmersiveGalleryAI/Common/PlayerLocation/PlayerLocationData.cs
@@ -1,5 +1,6 @@
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using Zenject;
 
 namespace ImmersiveGalleryAI.Common.PlayerLocation
@@ -7,14 +8,30 @@ namespace ImmersiveGalleryAI.Common.PlayerLocation
     public class PlayerLocationData : MonoBehaviour
     {
         [SerializeField] private XROrigin _xrOrigin;
+        [SerializeField] private LocomotionProvider _locomotionProvider;
 
         [Inject] private IPlayerLocation _playerLocation;
-        
+
         public Transform CameraRigTransform => _xrOrigin.Camera.transform;
 
         private void Start()
         {
             _playerLocation.Init(this);
+        }
+
+        private void OnEnable()
+        {
+            _locomotionProvider.endLocomotion += LocomotionProviderOnendLocomotion;
+        }
+
+        private void OnDisable()
+        {
+            _locomotionProvider.endLocomotion -= LocomotionProviderOnendLocomotion;
+        }
+
+        private void LocomotionProviderOnendLocomotion(LocomotionSystem obj)
+        {
+            _playerLocation.PlayerTeleported();
         }
     }
 }
--- Assets/ImmersiveGalleryAI/Common/PlayerLocation/PlayerLocationManager.cs
+++ Assets/ImmersiveGalleryAI/Common/PlayerLocation/PlayerLocationManager.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace ImmersiveGalleryAI.Common.PlayerLocation
 {
     public class PlayerLocationManager : IPlayerLocation 
     {
+        public event Action PlayerTeleportedEvent;
         public Transform CameraRigTransform { get; private set; }
 
         public void Init(PlayerLocationData playerLocationData)
         {
             CameraRigTransform = playerLocationData.CameraRigTransform;
+        }
+
+        public void PlayerTeleported()
+        {
+            PlayerTeleportedEvent?.Invoke();
         }
     }
 }
